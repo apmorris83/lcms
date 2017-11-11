94a8e4fc539ebba26be64a5f16276c195eb61ce4
Andrew Morris <apmorris83@gmail.com>
2017-11-11 16:43:23 +0000

    Add nav bar

--- www/js/index.js
+++ www/js/index.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
-    console.log('hello');
+    $(window).scroll(function() {
+        var headerHeight = $('#header').outerHeight() + 60;
+        var navHeight = $('#lcms-nav').outerHeight();
+        var scrollPos = $(window).scrollTop();
+        var windowWidth = $(window).width();
+    
+        if (scrollPos >= headerHeight && windowWidth > 767) {
+            $('#lcms-nav').addClass('nav-fixed');
+            $('#header').css('margin-top', navHeight);
+        } else {
+            $('#lcms-nav').removeClass('nav-fixed');
+            $('#header').css('margin-top', '0');
+        }
+    });
 });
+
