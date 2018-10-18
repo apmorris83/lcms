c3484f9d728366512f6ce1dbd9e39eee32db0055
Andrew Morris <apmorris83@gmail.com>
2018-10-18 22:07:42 +0100

    Add header and footer plus layout tweaks

--- www/js/index.js
+++ www/js/index.js
@@ -1,17 +1,46 @@
 $(document).ready(function(){
-    $(window).scroll(function() {
-        var headerHeight = $('#header').outerHeight() + 60;
-        var navHeight = $('#lcms-nav').outerHeight();
-        var scrollPos = $(window).scrollTop();
-        var windowWidth = $(window).width();
+    // $(window).scroll(function() {
+    //     var headerHeight = $('#header').outerHeight() + 60;
+    //     var navHeight = $('#lcms-nav').outerHeight();
+    //     var scrollPos = $(window).scrollTop();
+    //     var windowWidth = $(window).width();
     
-        if (scrollPos >= headerHeight && windowWidth > 767) {
-            $('#lcms-nav').addClass('nav-fixed');
-            $('#header').css('margin-top', navHeight);
+    //     if (scrollPos >= headerHeight && windowWidth > 767) {
+    //         $('#header').addClass('active');
+    //     } else {
+    //         $('#header').removeClass('active');
+    //     }
+    // });
+
+    // $(window).scroll(function() {
+    //     var hT = $('#services').offset().top,
+    //         hH = $('#services').outerHeight(),
+    //         wH = $(window).height(),
+    //         wS = $(this).scrollTop();
+    //     if (wS > (hT+hH-wH)){
+    //         $('#header').addClass('active');
+    //     } else {
+    //         $('#header').removeClass('active');
+    //     }
+    //  });
+
+     var element_position = $('#services').offset().top;
+
+     $(window).on('scroll', function() {
+         var y_scroll_pos = window.pageYOffset;
+         var scroll_pos_test = element_position;
+     
+         if(y_scroll_pos > scroll_pos_test) {
+            $('#header').addClass('active');
         } else {
-            $('#lcms-nav').removeClass('nav-fixed');
-            $('#header').css('margin-top', '0');
+            $('#header').removeClass('active');
         }
+     });
+
+    $('.find-out-more').click(function(){
+        $('html, body').animate({
+            scrollTop: $("#contact").offset().top
+        }, 500);
     });
 });
 
